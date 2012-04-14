3e775c2f38aee8858fba5813022d2973f8d532a8
Muralidhar <smuralireddy@gmail.com>
2012-04-14 17:25:50 +0530

    Update copter.java

--- copter.java
+++ copter.java
@@ -1,7 +1,4 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package game;
 
 import javax.microedition.midlet.*;
@@ -9,20 +6,17 @@ import javax.microedition.lcdui.*;
 import java.util.*;
 import javax.microedition.media.*;
 
-/**
- * @author mahi
- */
 public class copter extends MIDlet {
 GameDesign gd;
-boolean pause=false;
-Display d;
-//Player player;
+boolean pause = false;
+Display display;
+Player player;
  
-    public copter(){
-        gd=new GameDesign();
-        d=Display.getDisplay(this);
+    public copter() {
+        gd = new GameDesign();
+        display = Display.getDisplay(this);
         gd.start();
-        d.setCurrent(gd);/*
+        display.setCurrent(gd);
         try {
 	    player = Manager.createPlayer(getClass().getResourceAsStream("/game/res/theme.wav"),"audio/x-wav");
 	    player.setLoopCount(-1);
@@ -31,39 +25,37 @@ Display d;
 	catch(Exception e)
 	    {
 	    System.out.println("player"+e);
-	    }*/
-
-
-
+	    }
     }
 
     public void startApp() {
         if(pause){
-            pause=false;
+            pause = false;
         gd.resume();}
-        //System.out.println("back to copter");
-        //boolean b=gd.hasEnded();
         while(gd.hasEnded());
         if(!gd.hasEnded())
         {
-            System.out.println("ended");
            this.notifyDestroyed();
-           /*try{
-           player.stop();}
-           catch(Exception e){}*/
+           try
+           {
+           	player.stop();
+           }
+           catch(Exception e){}
            this.destroyApp(true);
         }    
         
     }
     
     public void pauseApp() {
-        try{
-        gd.pause();
-        pause=true;}
+        try
+        {
+        	gd.pause();
+        	pause = true;
+        }
         catch(Exception e){}
     }
     
     public void destroyApp(boolean unconditional) {
-        //Display.getDisplay (this).setCurrent ((Displayable) null);
+        
     }
 }
